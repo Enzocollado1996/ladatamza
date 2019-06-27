e6381570e05ea302eccc08b844c6ca96eb11c3bf
Jesus <sernajesus13@gmail.com>
2019-06-27 00:13:28 -0300

    ruta admin

--- config/routes.php
+++ config/routes.php
@@ -62,8 +62,12 @@ Router::scope('/', function (RouteBuilder $routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
-    //$routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+    //$routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);ç
+    /*** Rutas frontend ***/
     $routes->connect('/', ['controller' => 'Frontend', 'action' => 'index']);
+    
+    /*** Rutas backend ***/
+    $routes->connect('/admin', ['controller' => 'Users', 'action' => 'login']);
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
      */
